3f135819257360bb6ffc379d5331b6433a35e67e
AlexKnauth <alexander@knauth.org>
2018-05-18 19:46:06 -0400

    fill in the module hash with values

--- hackett-module/hackett-module/mod.rkt
+++ hackett-module/hackett-module/mod.rkt
@@ -48,14 +48,17 @@
                                  #'#%variable-reference)))
 
   (define-syntax-class hackett-module-component
+    #:attributes [sig-entry [val-id 1]]
+
     #:literal-sets [mod-stop-literals]
-    #:attributes [sig-entry]
     [pattern (hkt:: id:id {~type type:expr} {~optional #:exact})
-             #:with sig-entry #'(sig:val id : type)]
+             #:with sig-entry #'(sig:val id : type)
+             #:with [val-id ...] #'[id]]
     [pattern (hkt:type {~type spec} {~type rhs:expr})
              #:fail-unless (identifier? #'spec)
              "type aliases with arguments not allowed in modules"
-             #:with sig-entry #'(sig:type id = rhs)])
+             #:with sig-entry #'(sig:type spec = rhs)
+             #:with [val-id ...] #'[]])
 
   ;; Id -> Bool
   (define (variable-id? x)
@@ -100,39 +103,50 @@
 ; When the outer module is fully expanded, `mod/acc` will leave behind ???
 
 (begin-for-syntax
-  (define mod/acc-sig-prop
-    (gensym 'sig⇒))
+  (define mod/acc-sig-prop (gensym 'sig⇒))
+  (define mod/acc-vals-prop (gensym 'mod-vals))
 
   (define-syntax-class mod/acc-sig
-    #:attributes [sig]
+    #:attributes [sig [val-id 1]]
     [pattern stx
              #:attr sig (syntax-local-introduce
                          (syntax-property #'stx mod/acc-sig-prop))
-             #:when (attribute sig)]))
+             #:attr val-ids (syntax-local-introduce
+                             (syntax-property #'stx mod/acc-vals-prop))
+             #:when (attribute sig)
+             #:with [val-id ...] #'val-ids]))
 
 ; a module signature
 (define-syntax-parser mod/acc
-  [(_ [sig-entry/rev ...])
+  [(_ [sig-entry/rev ...] [val-id/rev ...])
    #:with [sig-entry ...] (reverse (attribute sig-entry/rev))
+   #:with val-ids (reverse (attribute val-id/rev))
    #:with s:sig #'(sig:sig sig-entry ...)
-   (syntax-property #'(define-values [] (values))
-     mod/acc-sig-prop
-     (syntax-local-introduce
-      (attribute s.expansion)))]
+   (syntax-property
+    (syntax-property #'(define-values [] (values))
+      mod/acc-sig-prop
+      (syntax-local-introduce
+       (attribute s.expansion)))
+    mod/acc-vals-prop
+    (syntax-local-introduce
+     #'val-ids))]
 
-  [(_ [ent/rev ...] defn rest-defn ...)
+  [(_ [ent/rev ...] [v/rev ...] defn rest-defn ...)
    #:with defn- (local-expand #'defn 'module mod-stop-ids)
    (syntax-parse #'defn-
      #:literal-sets [mod-stop-literals]
 
      [(begin form ...)
-      #'(mod/acc [ent/rev ...] form ... rest-defn ...)]
+      #'(mod/acc [ent/rev ...] [v/rev ...] form ... rest-defn ...)]
 
      [d:hackett-module-component
-      #'(begin defn- (mod/acc [d.sig-entry ent/rev ...] rest-defn ...))]
+      #'(begin
+          defn-
+          (mod/acc [d.sig-entry ent/rev ...] [d.val-id ... v/rev ...]
+                   rest-defn ...))]
 
      [:pass-through
-      #'(begin defn- (mod/acc [ent/rev ...] rest-defn ...))]
+      #'(begin defn- (mod/acc [ent/rev ...] [v/rev ...] rest-defn ...))]
 
      [{~and (head . _) :disallowed}
       (raise-syntax-error #f
@@ -145,6 +159,7 @@
    #:with modbeg- (local-expand #'(hkt:#%module-begin
                                    (mod/acc
                                     []
+                                    []
                                     defn ...))
                                 'module-begin
                                 '())
@@ -154,18 +169,14 @@
            post-defn- ...) #'modbeg-
 
    ;; TODO: produce identifiers to reference val definitions from the sig
-   #|
-   #:with [[sig-val-sym sig-val-id] ...]
-   (hash-map (sig-internal-ids (attribute m.sig)) list)
    #:with [[sig-val-sym/id ...] ...]
-   #'[['sig-val-sym sig-val-id] ...]
-   |#
+   #'[['m.val-id m.val-id] ...]
 
    (syntax-property #'(let ()
                         pre-defn- ...
                         post-defn- ...
                         ;; TODO: fill in references to the definitions into the hash
-                        (hash #|sig-val-sym/id ... ...|#))
+                        (hash sig-val-sym/id ... ...))
      'sig:
      (syntax-local-introduce
       (attribute m.sig)))])
