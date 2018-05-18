fc10cd618baea4338406986e815cba5c23022470
Milo Turner <iitalics@gmail.com>
2018-05-18 18:05:20 -0400

    Basic signature inference (slightly broken)

--- hackett-module/hackett-module/def.rkt
+++ hackett-module/hackett-module/def.rkt
@@ -13,7 +13,23 @@
 (provide
  def-module)
 
+(begin-for-syntax
+
+  (define (sig⇒ stx)
+    (define m-
+      (local-expand stx 'module-begin '()))
+    (define sig
+      (syntax-local-introduce
+       (syntax-property m- 'sig:)))
+    (list m- sig))
+
+  )
+
 (define-syntax-parser def-module
   [(_ name:id m:expr)
-   #:with m- (local-expand #'m 'module-begin '())
-   #'(pretty-write 'm-)])
+   #:with [m- sig] (sig⇒ #'m)
+   #'(begin
+       (printf "inferred: ")
+       (pretty-write 'sig)
+       (printf "\nexpanded module body: ")
+       (pretty-write 'm-))])
--- hackett-module/hackett-module/mod.rkt
+++ hackett-module/hackett-module/mod.rkt
@@ -41,23 +41,52 @@
     #:literal-sets [mod-stop-literals]
     [pattern ({~or define-values
                    define-syntaxes
-                   #%require
-                   #%provide
                    #%expression
+                   }
+              . _)])
+
+  (define-syntax-class disallowed
+    #:literal-sets [mod-stop-literals]
+    [pattern ({~or #%provide
+                   #%require
+                   begin-for-syntax
+                   module
+                   module*
                    }
               . _)])
 
   )
 
+; The mod/acc form parses and expands definitions using the "trampolining #%module-begin" technique;
+; we insert `begin` expressions so that an outer `#%module-begin` will handle the introduction of
+; bindings.
+;
+; When the outer module is fully expanded, `mod/acc` will leave behind ???
 
+(begin-for-syntax
+  (define mod/acc-sig-prop
+    (gensym 'sig⇒))
+
+  (define-syntax-class mod/acc-sig
+    #:attributes [sig]
+    [pattern stx
+             #:attr sig (syntax-local-introduce
+                         (syntax-property #'stx mod/acc-sig-prop))
+             #:when (attribute sig)]))
+
+; a module signature
 (define-syntax-parser mod/acc
   [(_ [sig-entry/rev ...])
    #:with [sig-entry ...] (reverse (attribute sig-entry/rev))
    #:with s:sig #'(sig:sig sig-entry ...)
-   #'(pretty-write 's.expansion)]
+   (syntax-property #'(define-values [] (values))
+     mod/acc-sig-prop
+     (syntax-local-introduce
+      (attribute s.expansion)))]
 
   [(_ [ent/rev ...] defn rest-defn ...)
    #:with defn- (local-expand #'defn 'module mod-stop-ids)
+   #:do [(printf "~s\n" #'defn-)]
    (syntax-parse #'defn-
      #:literal-sets [mod-stop-literals]
 
@@ -68,11 +97,31 @@
       #'(begin defn- (mod/acc [d.sig-entry ent/rev ...] rest-defn ...))]
 
      [:pass-through
-      #'(begin defn- (mod/acc [ent/rev ...] rest-defn ...))])])
+      #'(begin defn- (mod/acc [ent/rev ...] rest-defn ...))]
+
+     [{~and (head . _) :disallowed}
+      (raise-syntax-error #f
+        (format "~a form not allowed inside of a module"
+                #'head)
+        #'defn)])])
 
 (define-syntax-parser mod
   [(_ defn ...)
-   #'(hkt:#%module-begin
-      (mod/acc
-       []
-       defn ...))])
+   #:with modbeg- (local-expand #'(hkt:#%module-begin
+                                   (mod/acc
+                                    []
+                                    defn ...))
+                                'module-begin
+                                '())
+   #:with (mb-head:id
+           pre-defn- ...
+           m:mod/acc-sig
+           post-defn- ...) #'modbeg-
+
+   (syntax-property #'(let ()
+                        pre-defn- ...
+                        post-defn- ...
+                        (hash))
+     'sig:
+     (syntax-local-introduce
+      (attribute m.sig)))])
