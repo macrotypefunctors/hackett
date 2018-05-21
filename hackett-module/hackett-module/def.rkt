eb3b986d67567ed0944954a90d135ae56d507e04
AlexKnauth <alexander@knauth.org>
2018-05-21 11:20:03 -0400

    def-module defines a module id with a sig attached

--- hackett-module/hackett-module/def.rkt
+++ hackett-module/hackett-module/def.rkt
@@ -8,12 +8,16 @@
  (prefix-in hkt: hackett/base)
  (prefix-in sig: "sig.rkt")
  (for-syntax racket/base
-             syntax/parse))
+             racket/syntax
+             syntax/parse
+             hackett/private/util/stx))
 
 (provide
  def-module)
 
 (begin-for-syntax
+  (define (attach-sig stx s)
+    (syntax-property stx 'sig: s))
 
   (define (sig⇒ stx)
     (define m-
@@ -23,14 +27,24 @@
        (syntax-property m- 'sig:)))
     (list m- sig))
 
+  ;; Id Sig -> Transformer
+  (define (make-module-var-transformer x s)
+    (make-variable-like-transformer
+     ; Adjust source location information before calling attach-type so that tooltips end up in the
+     ; right place.
+     (λ (stx) (attach-sig (replace-stx-loc x stx) s))))
+
   )
 
 (define-syntax-parser def-module
   [(_ name:id m:expr)
    #:with [m- sig] (sig⇒ #'m)
+   #:with name- (generate-temporary #'name)
    #'(begin
        (printf "inferred: ")
        (pretty-write 'sig)
-       (define name m-)
+       (define-syntax name
+         (make-module-var-transformer (quote-syntax name-) (quote-syntax sig)))
+       (define name- m-)
        (printf "\nmodule body: ")
        (pretty-write name))])
