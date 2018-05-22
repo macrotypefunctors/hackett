672c6f5156be33d2a79da21074aa455eb24243d1
AlexKnauth <alexander@knauth.org>
2018-05-22 16:03:00 -0400

    add seal form

--- hackett-module/hackett-module/check/expand-check.rkt
+++ hackett-module/hackett-module/check/expand-check.rkt
@@ -1,11 +1,13 @@
 #lang racket/base
 (provide
  attach-sig
- sig⇒)
+ sig⇒
+ sig⇐)
 
 (require
  syntax/parse
  hackett/private/util/stx
+ "sig-matches.rkt"
  (for-template "../rep/sig.rkt"))
 
 (define (attach-sig stx s)
@@ -18,3 +20,15 @@
     (syntax-local-introduce
      (syntax-property m- 'sig:)))
   (list m- sig))
+
+;; assume expected is already expanded
+(define (sig⇐ stx expected [ctx #f])
+  (define/syntax-parse [m- actual] (sig⇒ stx ctx))
+
+  (unless (signature-matches? #'actual expected)
+    (raise-syntax-error #f
+      (format "signature mismatch\n  expected: ~a\n  given:    ~a"
+              (sig->string expected) (sig->string #'actual))
+      stx))
+  #'m-)
+
