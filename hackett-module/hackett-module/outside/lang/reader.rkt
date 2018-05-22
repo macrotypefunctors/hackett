fe552d19d8b44efd80b910a8232b7b08bf5526ec
Milo Turner <iitalics@gmail.com>
2018-05-22 10:56:53 -0400

    Added #%dot reader

--- hackett-module/hackett-module/outside/lang/reader.rkt
+++ hackett-module/hackett-module/outside/lang/reader.rkt
@@ -1,5 +1,5 @@
 #lang s-exp syntax/module-reader hackett-module/outside
-#:wrapper1 call-with-hackett-reading-parameterization
+#:wrapper1 call-with-hackett-module-reading-parameterization
 #:module-wrapper module-wrapper-insert-type-require
-(require hackett/private/reader
+(require hackett-module/reader
          (submod hackett/private/kernel module-wrapper))
