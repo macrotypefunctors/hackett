789841c46eaf49ad38591889e75a28ae6798d42b
Milo Turner <iitalics@gmail.com>
2018-05-22 17:42:40 -0400

    Delete hackett-module/inside lang

--- hackett-module/hackett-module/inside/lang/reader.rkt
+++
@@ -1,5 +0,0 @@
-#lang s-exp syntax/module-reader hackett-module/inside
-#:wrapper1 call-with-hackett-reading-parameterization
-#:module-wrapper module-wrapper-insert-type-require
-(require hackett/private/reader
-         (submod hackett/private/kernel module-wrapper))
