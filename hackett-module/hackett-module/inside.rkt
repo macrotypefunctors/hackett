a8530cb11fd2e86673c0de5092f47180f8e50bee
Milo Turner <iitalics@gmail.com>
2018-05-22 17:42:40 -0400

    Delete hackett-module/inside lang

--- hackett-module/hackett-module/inside.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-(require hackett/base
-         hackett/prelude
-         "mod.rkt")
-(provide
- (except-out (all-from-out hackett/base) #%module-begin)
- (all-from-out hackett/prelude)
- (rename-out [mod #%module-begin]))
--- hackett-module/hackett-module/test/test-inside.rkt
+++
@@ -1,8 +0,0 @@
-#lang hackett-module/inside
-
-(type X Integer)
-
-(defn foo : (∀ [a] {a -> (Tuple a X)})
-  [[x] (Tuple (: x a) 5)])
-
-{1 + 1}
