e6ed2b5539fa60bad92ef5ee763a5ae65dde0037
AlexKnauth <alexander@knauth.org>
2018-05-22 15:02:15 -0400

    implement #%dot_τ for getting types out of modules
    still need to "unsubstitute them" in the outputs of functor signatures, put the #%dot_τ back instead of the (#%type:con opaque:M.U34)

--- hackett-module/hackett-module/dot.rkt
+++ hackett-module/hackett-module/dot.rkt
@@ -13,12 +13,27 @@
              (only-in syntax/parse [attribute @])
              hackett/private/typecheck
              hackett/private/util/stx
-             "check/module-var.rkt"))
+             "check/module-var.rkt"
+             (for-syntax racket/base
+                         racket/syntax
+                         syntax/parse)))
+
+(begin-for-syntax
+  (define disappeared-use 'disappeared-use)
+
+  ;; puts it in the whichever namespace modules are in,
+  ;; which for now is the value namespace
+  (define-syntax ~module
+    (pattern-expander
+     (syntax-parser
+       [(_ pat)
+        #:with tmp (generate-temporary 'module-ns-tmp)
+        #'{~and tmp {~parse pat (value-namespace-introduce #'tmp)}}]))))
 
 (define-syntax-parser #%dot_e
   #:literal-sets [sig-literals]
 
-  [(_ m:module-binding ~! x:id)
+  [(_ {~module m:module-binding} ~! x:id)
    #:with m- #'m.internal-id
    #:do [(define decl
            (hash-ref (sig-decls #'m.sig)
@@ -28,9 +43,23 @@
    (format "not bound to a value in module ~a" (syntax-e #'m))
    #:with (#%val-decl t) decl
    #:with {~var t_qual (type (@ m.expansion-ctx))} #'t
-   (attach-type #'(#%app hash-ref m- 'x)
-                #'t_qual.expansion)])
+   (syntax-property
+    (attach-type #'(#%app hash-ref m- 'x)
+                 #'t_qual.expansion)
+    disappeared-use
+    (syntax-local-introduce #'m))])
 
 (define-syntax-parser #%dot_τ
-  [(_ m x)
-   (raise-syntax-error #f "#%dot for types" this-syntax)])
+  [(_ {~module m:module-binding} ~! x:id)
+   #:do [(define sym (syntax-e #'x))
+         (define internal-id
+           (hash-ref (sig-internal-ids #'m.sig) sym #f))
+         (define decl
+           (hash-ref (sig-decls #'m.sig) sym #f))]
+   #:fail-when (and (not (decl-type? decl)) #'x)
+   (format "not bound to a type in module ~a" (syntax-e #'m))
+   #:with {~var t_qual (type (@ m.expansion-ctx))} internal-id
+   (syntax-property
+    #'t_qual.expansion
+    disappeared-use
+    (syntax-local-introduce #'m))])
