e61dac032989dfe28dabd29c371f538c576d0fcf
Milo Turner <iitalics@gmail.com>
2018-05-23 12:00:39 -0400

    Refactor some logic into syntax-local-bind-module

--- hackett-module/hackett-module/rep/sig.rkt
+++ hackett-module/hackett-module/rep/sig.rkt
@@ -102,20 +102,12 @@
            #:with x- (generate-temporary #'x)
 
            ;; create a context where x is bound
-           #:do [(define bindings
-                   (generate-module-var-bindings
-                    #'x
-                    #'x-
-                    #'A.expansion))
-                 (define ctx (syntax-local-make-definition-context))
+           #:do [(define ctx (syntax-local-make-definition-context))
                  (define (intro stx)
                    (internal-definition-context-introduce ctx stx))
 
                  (syntax-local-bind-syntaxes (list #'x-) #f ctx)
-                 (syntax-local-bind-syntaxes
-                  (map first bindings)
-                  #`(values #,@(map second bindings))
-                  ctx)]
+                 (syntax-local-bind-module #'x #'x- #'A.expansion)]
 
            #:with x-- (intro #'x-)
            #:with {~var B* (sig ctx)} #'B
