d367ced2741e71a03a64a94be6c503b0c20328ac
Milo Turner <iitalics@gmail.com>
2018-05-23 17:28:01 -0400

    for/free-id-table

--- hackett-module/hackett-module/util/stx.rkt
+++ hackett-module/hackett-module/util/stx.rkt
@@ -1,9 +1,12 @@
 #lang racket/base
 
-(provide hash-literal
+(provide for/free-id-table
+         hash-literal
          (rename-out [hash-literal hash-lit]))
 
-(require syntax/parse)
+(require syntax/parse/define
+         syntax/parse
+         syntax/id-table)
 
 (define-syntax-class hash-literal
   [pattern stx
@@ -12,3 +15,8 @@
     #:attr keys (hash-keys (attribute value))
     #:attr values (hash-values (attribute value))])
 
+(define-simple-macro (for/free-id-table (clause ...) body ...)
+  (for/fold ([tbl (make-immutable-free-id-table)])
+            (clause ...)
+    (let-values ([(k v) (let () body ...)])
+      (free-id-table-set tbl k v))))
