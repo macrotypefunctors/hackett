9678755b1bc71cd56ca06810ba42c107115ef3a3
AlexKnauth <alexander@knauth.org>
2018-05-25 12:03:15 -0400

    write down some functor data / case notes

---
+++ hackett-module/hackett-module/test/notes/functor-data-case-notes.rkt
@@ -0,0 +1,28 @@
+#lang hackett-module/outside
+
+(def-module F
+  (λₘ ([M : (sig (data B T F))])
+    (mod
+     (def f
+       (λ (x)
+         (case x
+           [[M.T] 1]
+           [[M.F] 0]))))))
+==>
+(λ (M-)
+  (define-syntax M
+    (.... M- .... B T F constructor:M.T33 constructor:M.F34 ....))
+  (define-syntax constructor:M.T33 (....))
+  (define-syntax constructor:M.F34 (....))
+
+  ;; we need to set up `#%dot` and `M` so that something like
+  ;; `(#%dot M T)` can expand *in a pattern context* to
+  ;;  `constructor:M.T33`.
+  (define f
+    (λ (x)
+      (case x
+        [[(#%dot M T) 1]]
+        [[(#%dot M F) 0]])))
+  (hash 'f f))
+
+
