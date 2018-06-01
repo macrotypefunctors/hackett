f0ffe0e7c1face6e3e643bc76d269bf7cce6278f
Milo Turner <iitalics@gmail.com>
2018-06-01 13:33:00 -0400

    Change test-adt-with-args to use namespaces

--- hackett-module/hackett-module/test/test-adt-with-args.rkt
+++ hackett-module/hackett-module/test/test-adt-with-args.rkt
@@ -1,12 +1,15 @@
 #lang hackett-module/outside
+(require hackett/private/test)
 
 (def-module M
   (mod
-    (data Posn (MkPosn Integer Integer))))
+    (data Posn (Posn Integer Integer))))
 
-(def three-four
-  (M.MkPosn 3 4))
+(def three-four : M.Posn
+  (M.Posn 3 4))
 
 (def three
   (case three-four
-    [(M.MkPosn x y) x]))
+    [(M.Posn x y) x]))
+
+(test {three ==! 3})
