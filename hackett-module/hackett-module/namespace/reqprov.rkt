a7e760352ac307059e79bf0fc5581a0d304d8b50
AlexKnauth <alexander@knauth.org>
2018-06-04 17:22:45 -0400

    add (un)mangling require/provide transformers

---
+++ hackett-module/hackett-module/namespace/reqprov.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+
+(provide unmangle-in
+         type-out
+         module-out
+         signature-out)
+
+(require (for-syntax racket/base
+                     "namespace.rkt"
+                     "mangle/mangle-identifier.rkt"
+                     "mangle/mangle-reqprov.rkt"))
+
+(begin-for-syntax
+  (define-values [mangle-type unmangle-type]
+    (make-id-mangler #:prefix "#%hackett-type:"
+                     #:introducer type-namespace-introduce))
+
+  (define-values [mangle-module unmangle-module]
+    (make-id-mangler #:prefix "#%hackett-module:"
+                     #:introducer module-namespace-introduce))
+
+  (define-values [mangle-signature unmangle-signature]
+    (make-id-mangler #:prefix "#%hackett-signature:"
+                     #:introducer signature-namespace-introduce))
+
+  (define combined-unmangler
+    (or/unmangler unmangle-type
+                  unmangle-module
+                  unmangle-signature))
+  )
+
+(define-syntax unmangle-in
+  (make-unmangling-require-transformer combined-unmangler))
+
+(define-syntax type-out
+  (make-mangling-provide-transformer mangle-type))
+
+(define-syntax module-out
+  (make-mangling-provide-transformer mangle-module))
+
+(define-syntax signature-out
+  (make-mangling-provide-transformer mangle-signature))
+
