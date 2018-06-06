00e96c8d82b8d1a62eee6d12070243f3df385a1c
AlexKnauth <alexander@knauth.org>
2018-06-06 17:41:37 -0400

    put stx substitution in a utils file

--- hackett-module/hackett-module/util/stx.rkt
+++ hackett-module/hackett-module/util/stx.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (provide for/free-id-table
+         for*/free-id-table
          hash-literal
          (rename-out [hash-literal hash-lit]))
 
@@ -15,8 +16,25 @@
     #:attr keys (hash-keys (attribute value))
     #:attr values (hash-values (attribute value))])
 
-(define-simple-macro (for/free-id-table (clause ...) body ...)
-  (for/fold ([tbl (make-immutable-free-id-table)])
-            (clause ...)
-    (let-values ([(k v) (let () body ...)])
-      (free-id-table-set tbl k v))))
+(define-simple-macro
+  (define-accumulating-for-variant name base-for/fold ([acc init-expr] ...)
+    [(body-value ...) next])
+  (define-simple-macro (name clauses . body)
+    (base-for/fold ([acc init-expr] ...)
+                   clauses
+      (let-values ([(body-value ...) (let () . body)])
+        next))))
+
+(define-simple-macro
+  (define-accumulating-for/*-variant name name* ([acc init-expr] ...)
+    [(body-value ...) next])
+  (begin
+    (define-accumulating-for-variant name for/fold ([acc init-expr] ...)
+      [(body-value ...) next])
+    (define-accumulating-for-variant name* for*/fold ([acc init-expr] ...)
+      [(body-value ...) next])))
+
+(define-accumulating-for/*-variant for/free-id-table for*/free-id-table
+  ([tbl (make-immutable-free-id-table)])
+  [(k v) (free-id-table-set tbl k v)])
+
