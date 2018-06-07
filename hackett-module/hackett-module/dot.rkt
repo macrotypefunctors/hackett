f3e39d3e5e88f63c489503e2c6a08214cfcd656c
Milo Turner <iitalics@gmail.com>
2018-06-07 16:15:43 -0400

    Added stub #%dot form for modules

--- hackett-module/hackett-module/dot.rkt
+++ hackett-module/hackett-module/dot.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 
 (provide (all-from-out "dot/dot-e.rkt")
-         (all-from-out "dot/dot-t.rkt"))
+         (all-from-out "dot/dot-t.rkt")
+         (all-from-out "dot/dot-m.rkt"))
 
 (require "dot/dot-e.rkt"
-         "dot/dot-t.rkt")
+         "dot/dot-t.rkt"
+         "dot/dot-m.rkt")
