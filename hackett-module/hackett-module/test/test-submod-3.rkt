86efdfc3e0870b3047f605618f6fa94f5fe60552
Milo Turner <iitalics@gmail.com>
2018-06-11 19:07:40 -0400

    Non-Π test for broken bool submodule stuff

--- hackett-module/hackett-module/test/test-submod-3.rkt
+++ hackett-module/hackett-module/test/test-submod-3.rkt
@@ -4,12 +4,30 @@
   (sig
    (val yes? : Bool)))
 
+(def-module Yes
+  (mod
+    (def yes? True)))
+
+;; ---------------------------------------------------------
+
+(def-module Flag-Submod
+  (mod
+    (def-module Sub Yes)))
+
+(def-module Check1
+  (seal Flag-Submod
+        :>
+        (sig
+          (module Sub : FLAG))))
+
+;; ---------------------------------------------------------
+
 (def-module Put-Submod
   (λ ([F : FLAG])
     (mod
       (def-module Sub F))))
 
-(def-module Check
+(def-module Check2
   (seal Put-Submod
         :>
         (Π ([F : FLAG])
