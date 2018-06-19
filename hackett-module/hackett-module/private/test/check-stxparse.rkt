cd48a228c053c28f4aff564bdc7c7af48ae5bf15
AlexKnauth <alexander@knauth.org>
2018-06-19 13:28:30 -0400

    add check-stxparse testing form and ~hash pat expander

---
+++ hackett-module/hackett-module/private/test/check-stxparse.rkt
@@ -0,0 +1,41 @@
+#lang racket/base
+
+(provide check-stxparse)
+
+(require rackunit
+         syntax/parse
+         syntax/parse/define
+         syntax/location
+         syntax/srcloc
+         (for-syntax racket/base))
+
+(define-syntax-parser check-stxparse
+  [(_ actual-expr:expr
+      {~and {~seq stuff ...}
+            {~seq {~alt {~seq #:literals _}
+                        {~seq #:literal-sets _}
+                        {~seq #:datum-literals _}}
+                  ...}}
+      expected-pat
+      {~seq #:when pass-expr:expr}
+      ...)
+   #:with stx this-syntax
+   (quasisyntax/loc this-syntax
+     (let ([actual-val actual-expr])
+       (with-check-info*
+        (list (make-check-name 'check-stxparse)
+              (make-check-location
+               (build-source-location-list (quote-srcloc stx)))
+              (make-check-expression 'stx)
+              (make-check-actual actual-val)
+              (make-check-expected 'expected-pat))
+        #,(quasisyntax/loc this-syntax
+            (λ ()
+              #,(syntax/loc this-syntax
+                  (check-true (syntax-parse actual-val
+                                stuff ...
+                                [expected-pat
+                                 (and pass-expr ...)]
+                                [_
+                                 #false]))))))))])
+
--- hackett-module/hackett-module/util/stx.rkt
+++ hackett-module/hackett-module/util/stx.rkt
@@ -3,6 +3,7 @@
 (provide for/free-id-table
          for*/free-id-table
          hash-literal
+         ~hash
          (rename-out [hash-literal hash-lit])
          generate-temporaries/1num
          generate-temporary/1num)
@@ -14,7 +15,13 @@
          syntax/parse/define
          syntax/parse
          syntax/id-table
-         syntax/stx)
+         syntax/stx
+         seq-no-order
+         (for-syntax racket/base
+                     racket/syntax
+                     syntax/parse))
+
+;; ---------------------------------------------------------
 
 (define-syntax-class hash-literal
   [pattern stx
@@ -22,6 +29,22 @@
     #:when (hash? (attribute value))
     #:attr keys (hash-keys (attribute value))
     #:attr values (hash-values (attribute value))])
+
+(define (hash->list2-list hsh)
+  (hash-map hsh list))
+
+(define-syntax ~hash
+  (pattern-expander
+   (syntax-parser
+     ;; each entry-pat will be matched against [k v]
+     [(_ entry-pat ...)
+      #:with tmp (generate-temporary)
+      #:with tmp.value (format-id #'tmp "~a.~a" #'tmp 'value)
+      #'(~and {~var tmp hash-literal}
+              (~parse (~no-order entry-pat ...)
+                      (hash->list2-list (attribute tmp.value))))])))
+
+;; ---------------------------------------------------------
 
 (define-simple-macro
   (define-accumulating-for-variant name base-for/fold ([acc init-expr] ...)
@@ -44,6 +67,8 @@
 (define-accumulating-for/*-variant for/free-id-table for*/free-id-table
   ([tbl (make-immutable-free-id-table)])
   [(k v) (free-id-table-set tbl k v)])
+
+;; ---------------------------------------------------------
 
 ;; Any -> Symbol
 (define (anything->temporary-symbol x)
@@ -72,3 +97,6 @@
 ;; Any -> Id
 (define (generate-temporary/1num x)
   (first (generate-temporaries/1num (list x))))
+
+;; ---------------------------------------------------------
+
