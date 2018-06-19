c12892d9a946aa6458d74c707a1db7b462953ff3
Milo Turner <iitalics@gmail.com>
2018-06-19 13:17:46 -0400

    Better reinterpret

--- hackett-module/hackett-module/rep/reinterpret.rkt
+++ hackett-module/hackett-module/rep/reinterpret.rkt
@@ -1,41 +1,70 @@
 #lang racket/base
 
 (provide #%apply-type
-         (for-syntax reinterpret))
+         (for-syntax reinterpret
+                     u-type-literals
+                     ?#%apply-type
+                     path->u-type-path
+                     path->u-mod-path))
 
 (require
  syntax/parse/define
  "../namespace/reqprov.rkt"
  "../rep/sig-literals.rkt"
+ (except-in hackett/private/type-alias type)
  (except-in hackett/private/type-language
             value-namespace-introduce type-namespace-introduce ~type)
  (rename-in (unmangle-in "../dot/dot-m.rkt") [#%dot #%dot_m])
  (rename-in (unmangle-in "../dot/dot-t.rkt") [#%dot #%dot_τ])
+ (only-in (unmangle-in #:only hackett/base) #%app)
  (for-syntax racket/base
              racket/pretty
              (only-in syntax/parse [attribute @])
              syntax/parse/experimental/template
+             syntax/parse/class/local-value
+             syntax/apply-transformer
+             hackett/private/util/stx
              "../namespace/namespace.rkt"
              "../util/stx-traverse.rkt"
              "../util/disappeared-use.rkt"))
 
+
+(begin-for-syntax
+
+  (define-syntax-class applicable-type
+    #:attributes [apply]
+    [pattern {~var head (local-value alias-transformer?)}
+      #:attr apply
+      (λ (args)
+        (local-apply-transformer (@ head.local-value)
+                                 #`(head #,@args)
+                                 'expression))]
+
+    [pattern (#%dot_τ {~module m:dot-accessible-path/type} ~! x:id)
+      #:do [(define key (namespaced:type (syntax-e #'x)))
+            (define type-id ((@ m.key->id) key))]
+      #:fail-unless type-id
+      (format "~a is not bound to a type within ~a"
+              (syntax-e #'x)
+              (syntax->datum #'m))
+
+      #:with a:applicable-type type-id
+      #:attr apply (compose (λ (x) (add-disappeared-use x #'m.root))
+                            (@ a.apply))]
+
+    [pattern head
+      #:attr apply
+      (λ (args)
+        (define/syntax-parse [arg ...] args)
+        (template (?#%type:app* head arg ...)))])
+
+  )
+
 (define-syntax-parser #%apply-type
   #:literals [#%dot_τ]
-
-  [(_ head:id arg ...)
-   #'(head arg ...)]
-
-  [(_ (#%dot_τ {~module m:dot-accessible-path/type} ~! x:id) arg ...)
-   #:do [(define key (namespaced:type (syntax-e #'x)))
-         (define type-id ((@ m.key->id) key))]
-   #:fail-unless type-id
-   (format "~a is not bound to a type within ~a"
-           (syntax-e #'x)
-           (syntax->datum #'m))
-   #:with head type-id
-   (add-disappeared-use
-    #'(head arg ...)
-    #'m.root)])
+  [(_ head) #'head]
+  [(_ head:applicable-type arg ...+)
+   ((@ head.apply) (@ arg))])
 
 ;; A UType ("uninterpreted" type) is one of:
 ;;   - UTypeApp
@@ -63,6 +92,12 @@
     #:literal-sets [type-literals]
     [#%dot_τ #%dot_m #%apply-type])
 
+  (define-template-metafunction ?#%apply-type
+    (syntax-parser
+      [(_ a b ...)
+       (quasitemplate/loc/props this-syntax
+         (?#%type:app* (#%type:con #%apply-type) a b ...))]))
+
   (define-syntax-class u-type
     #:attributes [norm]
     #:literal-sets [u-type-literals]
@@ -74,7 +109,8 @@
     #:attributes [norm]
     #:literal-sets [u-type-literals]
     [pattern (~#%type:app* (#%type:con dot:#%dot_τ ~!) mp:u-module-path (#%type:con x:id))
-             #:with norm #'(dot mp.norm x)]
+             #:with stx this-syntax
+             #:with norm (datum->syntax #'stx (list #'dot #'mp.norm #'x) #'stx)]
     [pattern x:id
              #:with norm #'x])
 
@@ -82,7 +118,8 @@
     #:attributes [norm]
     #:literal-sets [u-type-literals]
     [pattern (~#%type:app* (#%type:con dot:#%dot_m ~!) mp:u-module-path (#%type:con x:id))
-             #:with norm #'(dot mp.norm x)]
+             #:with stx this-syntax
+             #:with norm (datum->syntax #'stx (list #'dot #'mp.norm #'x) #'stx)]
     [pattern x:id
              #:with norm #'x])
 
@@ -90,7 +127,11 @@
     #:attributes [norm]
     #:literal-sets [u-type-literals]
     [pattern (~#%type:app* (#%type:con app:#%apply-type ~!) tp:u-type-path arg:u-type ...)
-             #:with norm #'(app tp.norm arg.norm ...)])
+             #:with stx this-syntax
+             #:with norm
+             (datum->syntax #'stx
+                            (list* #'app #'tp.norm #'[arg.norm ...])
+                            #'stx)])
 
   ;; Reinterpret the given uninterpreted type, so that the resulting type
   ;; can be expanded into a valid Hackett type.
@@ -105,6 +146,29 @@
     (syntax-parse stx
       [u:u-type #'u.norm]
       [_ (traverse-stx/recur stx reinterpret)]))
+
+
+  (define (path->u-type-path p)
+    (syntax-parse p
+      #:literals [#%dot_τ]
+      [x:id #'x]
+      [(dot:#%dot_τ mp x)
+       #:with mp* (path->u-mod-path #'mp)
+       (template
+        (?#%type:app* (#%type:con dot)
+                      mp*
+                      (#%type:con x)))]))
+
+  (define (path->u-mod-path p)
+    (syntax-parse p
+      #:literals [#%dot_m]
+      [x:id #'x]
+      [(dot:#%dot_m mp x)
+       #:with mp* (path->u-mod-path #'mp)
+       (template
+        (?#%type:app* (#%type:con dot)
+                      mp*
+                      (#%type:con x)))]))
 
   )
 
