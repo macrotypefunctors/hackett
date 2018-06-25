81a66bdaf500ef2261d328f5307a818ff8d97cb3
AlexKnauth <alexander@knauth.org>
2018-06-25 13:30:44 -0400

    delete reintroduce-#%dot (not needed anymore)

--- hackett-module/hackett-module/dot/dot-t.rkt
+++ hackett-module/hackett-module/dot/dot-t.rkt
@@ -1,19 +1,13 @@
 #lang racket/base
 
-(provide (type-out #%dot)
-         (for-syntax reintroduce-#%dot))
+(provide (type-out #%dot))
 
 (require syntax/parse/define
          "../namespace/reqprov.rkt"
          "../rep/sig-literals.rkt"
          (only-in "dot-m.rkt" dot-accessible-path/type)
-         (only-in hackett/private/type-language #%type:con)
          (for-syntax racket/base
-                     racket/bool
                      "../namespace/namespace.rkt"
-                     "../prop-dot-accessible.rkt"
-                     "../prop-reintroducible-dot-type.rkt"
-                     "../util/stx-traverse.rkt"
                      "../util/disappeared-use.rkt"
                      (only-in syntax/parse [attribute @])))
 
@@ -30,37 +24,3 @@
     type-id
     #'m.root)])
 
-(begin-for-syntax
-
-  ;; Id ModulePrefix Signature IntDefCtx -> SignatureStx
-  ;; ASSUME s-to-reintro is already expanded
-  ;; - `m-dots-are-from-id` should be bound (in ctx or surrounding env) to
-  ;;   a `prop:dot-origin` struct which designates which references to reintroduce
-  ;;   #%dot for.
-  ;; - `m-to-prefix-id` is used as the prefix for the #%dot form that gets
-  ;;   reintroduced for every occurence in `s-to-reintro`.
-  (define (reintroduce-#%dot m-dots-are-from-id m-prefix s-to-reintro ctx)
-    (define origin-sym
-      (dot-origin-module-sym
-       (syntax-local-value m-dots-are-from-id #f ctx)))
-
-    (define/syntax-parse m-prefix*
-      m-prefix)
-
-    ;; determine which opaque cons to substitute by comparing
-    ;; their mod internal id's with the prefix, to see if we should
-    ;; insert prefixes for them.
-
-    (define (traverse stx)
-      (syntax-parse stx
-        #:literals [#%type:con]
-        [{~or (#%type:con {~var x (reintroducible-dot-type-id ctx)})
-              {~var x (reintroducible-dot-type-id ctx)}}
-         (if (symbol=? (@ x.module-sym) origin-sym)
-             (syntax/loc stx (#%dot m-prefix* x.external-sym))
-             stx)]
-
-        [_ (traverse-stx/recur stx traverse)]))
-
-    (traverse s-to-reintro))
-  )
